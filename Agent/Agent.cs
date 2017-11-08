094c2ff48f4ca47480702ba8fde88f3cc48f67a7
RickyDean <rickydean5@juno.com>
2017-11-08 17:36:42 -0500

    Update Agent.cs

--- Agent/Agent.cs
+++ Agent/Agent.cs
@@ -8,11 +8,35 @@ namespace IntelligentVacuum.Agent
     {
         public Agent()
         {
+            //This is not working right, but was an effort.
+            int x = room.XAxis;
+            int y = room.YAxis;
+            AgentAction.action;
+            // Sense
+            bool isDirty = room.IsDirty;
+            //Plan
+            if(isDirty)
+            {
+                 action = AgentAction.CLEAN;
+            }
+            else
+            {
+                for (y = 1; y< room.YAxis; y--)
+                {
+                    if (x < room.XAxis)
+                    {
+                        action = AgentAction.MOVE_RIGHT;
+                    }
+                    else
+                        {
+                        action = AgentAction.MOVE_DOWN
+                    }
+                    return action;
         }
 
-        public AgentAction DecideAction(Room room)
-        {
-            return AgentAction.NONE;
-        }
+        //public AgentAction DecideAction(Room room)
+        //{
+           // return AgentAction.NONE;
+        //}
     }
 }
